2081f5dd47d259687f53e6e4e8252a298540de95
Nelson Nobre <technobre@gmail.com>
2021-11-21 19:46:42 +0000

    Start project

---
+++ src/Attributes/ResponseTypes/Response204NoContent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PowerUtils.AspNetCore.WebAPI.Attributes.ResponseTypes
+{
+    public class Response204NoContent : ProducesResponseTypeAttribute
+    {
+        public Response204NoContent() : base(StatusCodes.Status204NoContent) { }
+    }
+}
---
+++ src/Attributes/ResponseTypes/Response400BadRequest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PowerUtils.AspNetCore.WebAPI.ProblemDetailsHandlers;
+
+namespace PowerUtils.AspNetCore.WebAPI.Attributes.ResponseTypes
+{
+    public class Response400BadRequest : ProducesResponseTypeAttribute
+    {
+        public Response400BadRequest() : base(typeof(ProblemDetailsResponse), StatusCodes.Status400BadRequest) { }
+    }
+}
